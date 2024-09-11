cd23cac7a308f6e5d0070fb8e709bdba9acc15e3
Frank van Puffelen <frank.vanpuffelen@gmail.com>
2024-09-11 13:47:26 -0700

    Include content if there's no description
    If the note has no description in its frontmatter and the rendered content is not too long, include that as the description.

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -2,15 +2,12 @@ import rss from '@astrojs/rss';
 import { getNotes, getTypeForNote } from '../components/Notes.astro';
 import { renderStars } from '../components/StarRating.astro';
 
-// import sanitizeHtml from 'sanitize-html';
-// import MarkdownIt from 'markdown-it';
-// const parser = new MarkdownIt();
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
 export async function GET(context) {
-  const notes = await getNotes({ keepIf: (note) => note.props.frontmatter?.title?.length > 0 && note.params.pubDate?.length > 0 && !/readme.md$/.test(note.props.file) });
-
-  console.log("Notes for RSS", notes);
-  console.log("Notes with description: ", notes.filter((note) => note.props.frontmatter?.description).map((note) => note.props.Content));
+  const notes = (await getNotes({ keepIf: (note) => note.props.frontmatter?.title?.length > 0 && note.params.pubDate?.length > 0 && !/readme.md$/.test(note.props.file) }));
 
   return rss({
     title: 'puf.io',
@@ -22,16 +19,19 @@ export async function GET(context) {
       if (type === 'books') {
         title += ` - ${note.props.frontmatter.author} (Rating: ${renderStars(note.props.frontmatter?.rating)})`;
       }
-      return {
+      let item = {
         title: title,
         link: note.params.slug,
         pubDate: note.params.pubDate,
         description: note.props.frontmatter?.description,
-        //content: note.props.compiledContent,
-        // content: sanitizeHtml(note.props.compiledContent, {
-        //   allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
-        // }) + "\n\n",
       };
+      if (!item.description || item.description.length === 0) {
+        const content = sanitizeHtml(note.props.compiledContent(), {
+          allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+        });
+        if (content.length < 5_000) item.description = content + "\n\n";
+      }
+      return item;
     }),
     customData: `<language>en-us</language>`,
   });
