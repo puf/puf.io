38468731b59b7b148a52bf27b10810a40a5d566e
puf <frank.vanpuffelen@gmail.com>
2025-04-24 14:27:23 -0700

    Update feed.xml.js
    Include longer content, as I am not good at writing summaries.
    
    Exclude content that is older than a year.

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -7,7 +7,15 @@ import MarkdownIt from 'markdown-it';
 const parser = new MarkdownIt();
 
 export async function GET(context) {
-  const notes = (await getNotes({ keepIf: (note) => note.props.frontmatter?.title?.length > 0 && note.params.pubDate?.length > 0 && !/readme.md$/.test(note.props.file) }));
+  const notes = (
+    await getNotes({ keepIf: (note) => 
+      note.props.frontmatter?.title?.length > 0 && 
+      note.params.pubDate?.length > 0 && 
+      !/readme.md$/.test(note.props.file) && 
+      !/^_/.test(note.props.file.split('/').pop()) &&
+      new Date(note.params.pubDate) > new Date(Date.now() - 365 * 24 * 60 * 60 * 1000)
+    })
+  );
 
   return rss({
     title: 'puf.io',
@@ -15,6 +23,7 @@ export async function GET(context) {
     site: context.site,
     items: notes.map((note) => {
       const type = getTypeForNote(note.props);
+      console.log(note.props.file);
       let title = note.props.frontmatter.title;
       if (type === 'books') {
         title += ` - ${note.props.frontmatter.author} (Rating: ${renderStars(note.props.frontmatter?.rating)})`;
@@ -38,7 +47,12 @@ export async function GET(context) {
               allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
             });
           }
-          if (content.length < 20_000) item.description = content + "\n\n";
+          if (content.length < 50_000) {
+            item.description = content + "\n\n";
+          }
+          else {
+            console.warn(`Skipping content for ${note.params.slug} in feed.xml, as it's too long (${content.length} chars)`);
+          }
         } catch (e) {
           // Note: by catching the error, we still include the item but without its main content
           console.error(`Failed to sanitize content for ${note.params.slug}`, e);
